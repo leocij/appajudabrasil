5eef5b3ae4713d89aaf6a1323f3ce0d6be8d9def
Leoci Melo <leocij@hotmail.com>
2019-07-31 21:45:42 -0300

    Workshop finalizado

---
+++ app/src/main/java/br/com/lemelosoft/appajudabrasil/MainActivity.kt
@@ -0,0 +1,63 @@
+package br.com.lemelosoft.appajudabrasil
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+
+class MainActivity : AppCompatActivity() {
+
+    var list: MutableList<Proposicao> = emptyList<Proposicao>().toMutableList()
+    lateinit var recyclerView: RecyclerView
+    lateinit var adapter: ProposicoesAdapter
+    lateinit var requestQueue: RequestQueue
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestQueue = Volley.newRequestQueue(this)
+
+        recyclerView = findViewById(R.id.rv_proposicoes)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        adapter = ProposicoesAdapter(list, this, requestQueue)
+        recyclerView.adapter = adapter
+
+        load()
+    }
+
+    private fun load() {
+        val URI = "https://dadosabertos.camara.leg.br/api/v2/proposicoes?ordem=ASC&ordenarPor=id"
+        val request = JsonObjectRequest(
+            Request.Method.GET,
+            URI,
+            null,
+            Response.Listener<JSONObject> {
+                list.clear()
+                val dados = it.getJSONArray("dados")
+                for (i in 0 until dados.length()) {
+                    val item = dados.getJSONObject(i)
+                    list.add(
+                        Proposicao(
+                            item.getInt("id"),
+                            item.getString("uri"),
+                            item.getString("siglaTipo"),
+                            item.getInt("ano"),
+                            item.getString("ementa")
+                        )
+                    )
+                }
+
+                adapter.notifyDataSetChanged()
+            },
+            null)
+        requestQueue.add(request)
+    }
+}
---
+++ app/src/main/java/br/com/lemelosoft/appajudabrasil/Proposicao.kt
@@ -0,0 +1,9 @@
+package br.com.lemelosoft.appajudabrasil
+
+data class Proposicao(
+    var id: Int,
+    var uri: String,
+    var siglaTipo: String,
+    var ano: Int,
+    var ementa: String
+)
